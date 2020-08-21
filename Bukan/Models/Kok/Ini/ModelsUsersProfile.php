26f4b68d416694898c8faa43247d5b15cdd2f014
Mulia Arifandi Nasution <mul14.net@gmail.com>
2020-08-21 17:13:50 +0700

    First commit

---
+++ Bukan/Models/Kok/Ini/ModelsUsersProfile.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Apapun\Beda\Gak\Ngaruh\Dengan\Nama\Folder;
+
+class Profile
+{
+
+    public function __construct()
+    {
+        echo 'Profile dipanggil' . PHP_EOL;
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require('./Bukan/Models/Kok/Ini/ModelsUsersProfile.php');
+
+use Apapun\Beda\Gak\Ngaruh\Dengan\Nama\Folder\Profile;
+
+new Profile(); // Output: "Profile dipanggil"
